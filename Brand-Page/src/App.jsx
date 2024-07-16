009510c821f398e412434c38f032fab097234a34
Aditya Kumar Mishra <adityamishra17032003@gmal.com>
2024-07-16 11:08:28 +0530

    first commit

---
+++ Brand-Page/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navigation from './component/Navigation'
+import HeroSection from './component/Hero'
+
+function App() {
+
+  return (
+    <>
+     <Navigation/>
+     <HeroSection/>
+    </>
+  )
+}
+
+export default App
